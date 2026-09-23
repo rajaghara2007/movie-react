24041b829b7f0842a550eb5b7c8d2d0ec3ed9585
Raj Aghara <raj.aghara2007@gmail.com>
2026-09-23 12:42:05 +0530

    project done

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import { FavoritesProvider } from './context/FavoritesContext'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import MovieDetails from './pages/MovieDetails'
@@ -8,15 +9,17 @@ import About from './pages/About'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movie/:id" element={<MovieDetails />} />
-        <Route path="/favorites" element={<Favorites />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
+    <FavoritesProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:id" element={<MovieDetails />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </FavoritesProvider>
   )
 }
 
